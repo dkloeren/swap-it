055e4a3bab51c1371ff0351fc54826cb58d1143c
David Kloeren <d.kloeren@googlemail.com>
2021-07-28 13:15:02 +0200

    init factory and rspec for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    first_name {'Dom'}
+    last_name {'Inant'}
+    role {['admin','slave'].sample}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password}
+    department
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:department) { create(:department) }
+
+  before(:each) do
+    @user = create(:user, department: department)
+  end
+
+  it 'should return a valid user' do
+    expect(@user).to be_present
+  end
+
+  it 'should validate for email' do
+    # build: test creation of a model
+    user2 = build(:user, email: 'invalid_email')
+    expect(user2).to_not be_valid
+  end
+
 end
